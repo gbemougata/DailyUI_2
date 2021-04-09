cd2b2d1026cef181fc80100e97818d90bba937f4
gbemougata <30643036+gbemougata@users.noreply.github.com>
2021-04-09 17:55:54 -0400

    set up logo

---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="Logo-main">
+        <img src="assets/dailysmartylogo.png" alt="daily smarty logo"></img>
+      </div>
+    );
+  }
+}
+
+export default Logo;
