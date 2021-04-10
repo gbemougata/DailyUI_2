1e9eac8dabe286762944318650048d1b4bea1fb6
gbemougata <30643036+gbemougata@users.noreply.github.com>
2021-04-09 22:49:22 -0400

    setup search bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Logo from "./logo";
+import SearchBar from "./searchBar";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <Logo/>
-        
+      <div className="app">
+        <Logo />
+        <SearchBar />
       </div>
     );
   }
