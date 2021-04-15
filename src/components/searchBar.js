36d1505f3a67a62ac58cbf67f4a458609c66a324
gbemougata <30643036+gbemougata@users.noreply.github.com>
2021-04-15 15:05:50 -0400

    another push

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+
 import { withRouter } from "react-router-dom";
 
 class SearchBar extends Component {
   handleFormSubmit = function({ query }) {
     console.log("trying to handle submit for query", query);
-    // navigating to new route
-    this.pros.history.push("/Results");
+    this.props.history.push("/results");
   };
+
   renderInput(field) {
     return (
       <input type="text" placeholder="Search DailySmarty" {...field.input} />
     );
   }
+
   render() {
     const { handleSubmit } = this.props;
+
     return (
       <form
         className="search-bar"
@@ -29,5 +32,7 @@ class SearchBar extends Component {
 SearchBar = reduxForm({
   form: "searchBar"
 })(SearchBar);
+
 SearchBar = withRouter(SearchBar);
+
 export default SearchBar;
