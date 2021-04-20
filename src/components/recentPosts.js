73b0ed1e3674b3a35f57933a46cc0f5f1da7aa74
gbemougata <30643036+gbemougata@users.noreply.github.com>
2021-04-19 21:12:45 -0400

    origine

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,29 +1,24 @@
 import React, { Component } from "react";
-class RecentPosts extends Component{
-    render(){
-        return(
-            <div className="recent-posts">
-            <div className="recent-posts-wrapper">
-                <div className="recent_posts_ heading">recent posts</div>
-                <ul className="recent-post_posts">
-                <li>
-                    recent post 0
-                </li>
-                <li>
-                    recent post 1
-                </li>
-                <li>
-                    recent post 2
-                </li>
-
-
-
-                </ul>
-            </div>
-
-            </div>
-        )
-    }
+import { connect } from "react-redux";
+import * as actions from "../actions";
+class RecentPosts extends Component {
+  componentDidMount() {
+    this.props.fetchRecentPosts;
+  }
+  render() {
+    return (
+      <div className="recent-posts">
+        <div className="recent-posts-wrapper">
+          <div className="recent_posts_ heading">recent posts</div>
+          <ul className="recent-post_posts">
+            <li>recent post 0</li>
+            <li>recent post 1</li>
+            <li>recent post 2</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default RecentPosts;
+export default connect(null, actions)(SearchBar);
